ffe07c9f2fff755b832a93d540eb89ee16a58d21
serhii <serhiy.loboda@gmail.com>
2025-01-09 09:50:03 +0200

    update(route): added health_check endpoint

--- routes/main.py
+++ routes/main.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from flask_swagger_ui import get_swaggerui_blueprint
 
 
 main_route = Blueprint("main", __name__)
 
-SWAGGER_URL = "/swagger"
+SWAGGER_URL = "/swagger/"
 API_URL = "/static/swagger.yaml"
 
 swagger_ui_blueprint = get_swaggerui_blueprint(
@@ -13,3 +13,8 @@ swagger_ui_blueprint = get_swaggerui_blueprint(
     config={"app_name": "Access API"}
 )
 main_route.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+
+@main_route.route("/health_check/", methods=["GET"])
+def health_check():
+    return jsonify({"message": "API working"}), 200
