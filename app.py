c1c7743210505ac9fbc340834a1f9ca8c29d3e1d
serhii <serhiy.loboda@gmail.com>
2025-01-09 09:50:40 +0200

    create(app file): create first app

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_migrate import Migrate
+
+from config import Configurator
+from models import db
+from routes.main import main_route
+
+
+def create_app():
+    app = Flask(__name__, static_folder="static")
+
+    Configurator.init_flask_config(app=app)
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
+        migrate = Migrate(app, db)
+
+    app.register_blueprint(main_route)
+
+    return app
+
+application = create_app()
+
+if __name__ == "__main__":
+    application.run(debug=True, host="0.0.0.0", port=5000)
