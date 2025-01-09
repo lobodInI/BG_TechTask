bdb529cedf5cef60ecf449b17405bcacde9da842
serhii <serhiy.loboda@gmail.com>
2025-01-09 13:42:10 +0200

    update(models): added field 'owner' to TradingStrategy

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import enum
 
-from sqlalchemy import String, JSON, Enum
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, JSON, Enum, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.inspection import inspect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -21,6 +21,7 @@ class User(db.Model):
     last_name: Mapped[str] = mapped_column(String(30), nullable=False)
     email: Mapped[str] = mapped_column(String(30), nullable=False, unique=True, index=True)
     password: Mapped[str] = mapped_column(String(256), nullable=False)
+    strategies = relationship("TradingStrategy", back_populates="owner")
 
     def to_json(self) -> dict[str, int | str]:
         return {
@@ -35,7 +36,7 @@ class User(db.Model):
 
 
 class TradingStrategy(db.Model):
-    __tablename__ = "trading_strategy"
+    __tablename__ = "trading_strategies"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(150), nullable=False)
@@ -44,6 +45,9 @@ class TradingStrategy(db.Model):
     buy_condition: Mapped[JSON] = mapped_column(JSON, nullable=False)
     sell_condition: Mapped[JSON] = mapped_column(JSON, nullable=False)
     status: Mapped[TradingStatus] = mapped_column(Enum(TradingStatus), nullable=False, default=TradingStatus.active)
+
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped[User] = relationship(back_populates="strategies")
 
     def to_json(self) -> dict:
         return {column.key: getattr(self, column.key) for column in inspect(self).mapper.columns}
