1c009480e94e3b6c3f7a74666e46ae5220a9ca55
serhii <serhiy.loboda@gmail.com>
2025-01-10 12:21:46 +0200

    fix(naming): fix name config data to connect postgresql

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ class Configurator:
         app.config["SECRET_KEY"] = env("SECRET_KEY")
 
         sql_driver = env("SQL_DRIVER")
-        db_host = env("DB_HOST")
-        db_port = env("DB_PORT")
-        db_user = env("DB_USER")
-        db_pass = env("DB_PASSWORD")
-        db_name = env("DB_NAME")
+        db_host = env("POSTGRES_HOST")
+        db_port = env("POSTGRES_PORT")
+        db_user = env("POSTGRES_USER")
+        db_pass = env("POSTGRES_PASSWORD")
+        db_name = env("POSTGRES_DB")
 
         app.config["SQLALCHEMY_DATABASE_URI"] = (f"{sql_driver}://{db_user}:{db_pass}"
                                                  f"@{db_host}:{db_port}/{db_name}")
